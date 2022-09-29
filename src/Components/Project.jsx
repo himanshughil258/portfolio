e90af2c2665ddbee75e012b889a81ca04d38c713
Himanshughil258 <himanshughil258@gmail.com>
2022-09-29 20:28:48 +0530

    minor refactor

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -17,7 +17,7 @@ const Project = () => {
      name="project"
      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
-        <div className="py-52">
+        <div className="pt-56">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">Projects</p>
           <p className='py-6'>Check out some of my projects here</p>
         </div>
